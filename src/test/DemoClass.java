1f5c874e645716b20d61ab7802afb3cfb9ad19ed
sharifarna <sharifarna@gmail.com>
2023-05-10 17:40:50 +0530

    Second commit maded

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -6,6 +6,8 @@ public class DemoClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("This is Demo class from test package of Demo Project1");
+		System.out.println("This is the Second line added to Demo Project");
+	
 	}
 
 }
